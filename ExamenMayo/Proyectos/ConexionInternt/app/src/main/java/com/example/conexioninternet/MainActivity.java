9bb9479acda7e5620cece5e9ce36ca9ec44d07d2
Jose Ace <joseacevp@gmail.com>
2024-04-27 12:32:22 +0200

    Proyecto 1

--- ExamenMayo/Proyectos/ConexionInternt/app/src/main/java/com/example/conexioninternet/MainActivity.java
+++ ExamenMayo/Proyectos/ConexionInternt/app/src/main/java/com/example/conexioninternet/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.conexioninternet;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.conexioninternet.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //metodo comprueba el acceso a Internet
+                comprobarAccesoInternt();
+            }
         });
+
+
+    }
+
+    private void comprobarAccesoInternt() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(getApplicationContext().CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Toast.makeText(this, "Acceso internet OK", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "SIN acceso Internet", Toast.LENGTH_LONG).show();
+        }
     }
 }
