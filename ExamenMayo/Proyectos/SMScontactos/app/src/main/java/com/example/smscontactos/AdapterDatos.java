5476025c12d0aaac0f869e0bc53d30b0e71c4e6a
Jose Ace <joseacevp@gmail.com>
2024-05-01 08:26:53 +0200

    SMS Contactos OK
    Falta por implementar Buscador de contactos.
    Carga lista de contactos en RECYCLERVIEW.

---
+++ ExamenMayo/Proyectos/SMScontactos/app/src/main/java/com/example/smscontactos/AdapterDatos.java
@@ -0,0 +1,80 @@
+package com.example.smscontactos;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDatos> {
+
+    //1. lista de datos con los datos de los intem
+    ArrayList<String> listDatos;
+
+    //2. construptor de la lista de datos llega lista de datos y se
+    //asignan al elemento listDatos
+    public AdapterDatos(ArrayList<String> listDatos) {
+        this.listDatos = listDatos;
+    }
+
+    public class ViewHolderDatos extends RecyclerView.ViewHolder {
+        //3. referencia de los componentes de la lista de datos
+        TextView dato;
+        public final View view;
+
+        public ViewHolderDatos(@NonNull View itemView) {
+            super(itemView);
+            //4. instancia a la referencia de los datos de la lista de datos
+            dato = itemView.findViewById(R.id.texto_nombre);
+            view = itemView;
+        }
+
+    //8. asigna los datos del intem que toca de la lista y lo asigna
+    //al componente del xml listaItem
+        public void setAsignarDatos(String s) {
+            dato.setText(s);
+        }
+    }
+
+    /*
+    enlaza este adaptador con el xml de los items
+     */
+    @NonNull
+    @Override
+    public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        //5. infla la vista con la referencia del xml lista intem que contien la plantilla
+        //de los datos que reflejaran los componentes del recycleView
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.linea_contacto, null, false);
+        return new ViewHolderDatos(view);
+    }
+
+    //6.retorna el tamaño de la lista de datos recibida en el construptor
+    @Override
+    public int getItemCount() {
+        return listDatos.size();
+    }
+
+    //7. establece la comunicación entre el adaptador y la clase viewHolderDatos
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderDatos viewHolderDatos, int i) {
+        //llamamos al metodo asignardatos pasando el dato del intem que
+        //toca de la lista
+        viewHolderDatos.setAsignarDatos(listDatos.get(i));
+        //metodo viewHolderDatospare gestionar el item seleccionado
+        viewHolderDatos.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.reenviarDatos(viewHolderDatos.dato);
+            }
+        });
+    }
+
+
+}
+
---
+++ ExamenMayo/Proyectos/SMScontactos/app/src/main/java/com/example/smscontactos/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.smscontactos;
+
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.smscontactos.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    ArrayList<String> listaDatos;
+
+    private boolean tengo_permisos = false;
+    private final int PETICION_PERMISOS = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+// Solicitud de permisos
+        if (checkSelfPermission("android.permission.READ_CONTACTS") != PackageManager.PERMISSION_GRANTED
+                ||
+                checkSelfPermission("android.permission.SEND_SMS") != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{
+                            "android.permission.READ_CONTACTS",
+                            "android.permission.SEND_SMS"},
+                    PETICION_PERMISOS);
+        } else tengo_permisos = true;
+
+
+       listaDatos = buscarContacto(binding.editextBuscar.getText().toString());
+
+        binding.reciclerContactos.setLayoutManager(new LinearLayoutManager(this
+                , LinearLayoutManager.VERTICAL, false));
+        //15. enviamos la lista con los datos al adaptador
+        AdapterDatos adapter = new AdapterDatos(listaDatos);
+        //16. indicamos al recycleView que
+        binding.reciclerContactos.setAdapter(adapter);
+    }
+
+    public static void reenviarDatos(TextView dato) {
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==PETICION_PERMISOS)
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                tengo_permisos=true;
+            else
+                tengo_permisos=false;
+    }
+
+    @SuppressLint("Range")
+    private ArrayList<String> buscarContacto(String contacto) {
+        String proyeccion[] = {
+                ContactsContract.Contacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.Contacts.HAS_PHONE_NUMBER,
+                ContactsContract.Contacts.PHOTO_ID
+        };
+//        String filtro = ContactsContract.Contacts.DISPLAY_NAME + " like ?";
+//        String args_filtro[] = {"%" + contacto + "%"};
+
+        ArrayList<String> lista_contactos = new ArrayList<>();
+        ContentResolver contentResolver = getContentResolver();
+//        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+//                proyeccion, filtro, args_filtro, null);
+  Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                proyeccion, null, null, null);
+
+
+
+        // Con el cursor, recorrer la lista de contactos extraída, agregando los elementos a un ArrayList
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                // Obtener el nombre del contacto
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                // Si tiene teléfono lo agregamos a la lista de contactos
+                if (Integer.parseInt(cursor.getString(
+                        cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    lista_contactos.add(name);
+                }
+            } // FIN while
+        } // FIN if
+
+        cursor.close();
+        return lista_contactos;
+    }
+}
