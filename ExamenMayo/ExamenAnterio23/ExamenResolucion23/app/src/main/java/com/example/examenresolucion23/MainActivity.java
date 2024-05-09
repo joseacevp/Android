bafb34706b89b64938b8af4f4cee48cb3d1b753f
Jose Ace <joseacevp@gmail.com>
2024-05-09 19:56:47 +0200

    cargar contactos reales ok

--- ExamenMayo/ExamenAnterio23/ExamenResolucion23/app/src/main/java/com/example/examenresolucion23/MainActivity.java
+++ ExamenMayo/ExamenAnterio23/ExamenResolucion23/app/src/main/java/com/example/examenresolucion23/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.examenresolucion23;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,10 +24,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.examenresolucion23.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private boolean tengo_permisos = false;
+    private final int PETICION_PERMISOS = 1;
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private ArrayList<String> listaDatos, listaResultados;
+
+    public static void reenviarDatos(TextView nombre) {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +44,22 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        // Solicitud de permisos
+        if (checkSelfPermission("android.permission.READ_CONTACTS") != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission("android.permission.WRITE_CONTACTS") != PackageManager.PERMISSION_GRANTED
+                ||
+                checkSelfPermission("android.permission.SEND_SMS") != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{
+                            "android.permission.READ_CONTACTS",
+                            "android.permission.SEND_SMS"},
+                    PETICION_PERMISOS);
+        } else tengo_permisos = true;
+        if (tengo_permisos) {
+            Toast.makeText(this, "tengo permisos", Toast.LENGTH_SHORT).show();
+        }
+        // Fin Solicitud de permisos
+
 
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
@@ -62,5 +95,15 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PETICION_PERMISOS)
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                tengo_permisos = true;
+            else
+                tengo_permisos = false;
     }
 }
--- ExamenMayo/ExamenAnterio23/ExamenResolucion23/app/src/main/java/com/example/examenresolucion23/ui/cargar/CargarFragment.java
+++ ExamenMayo/ExamenAnterio23/ExamenResolucion23/app/src/main/java/com/example/examenresolucion23/ui/cargar/CargarFragment.java
@@ -1,6 +1,10 @@
 package com.example.examenresolucion23.ui.cargar;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,12 +12,19 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.examenresolucion23.AdapterDatos;
 import com.example.examenresolucion23.databinding.FragmentCargarBinding;
+
+import java.util.ArrayList;
 
 public class CargarFragment extends Fragment {
 
     private FragmentCargarBinding binding;
+    private boolean tengo_permisos = false;
+    private final int PETICION_PERMISOS = 1;
+    private ArrayList<String> listaDatos, listaResultados;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,7 +34,13 @@ public class CargarFragment extends Fragment {
         binding = FragmentCargarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
+        listaDatos = llenarContactos();
+        binding.recyclerContactosReales.setLayoutManager(new LinearLayoutManager(getContext()
+                , LinearLayoutManager.VERTICAL, false));
+        //15. enviamos la lista con los datos al adaptador
+        AdapterDatos adapter = new AdapterDatos(listaDatos);
+        //16. indicamos al recycleView que
+        binding.recyclerContactosReales.setAdapter(adapter);
         return root;
     }
 
@@ -32,4 +49,35 @@ public class CargarFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    @SuppressLint("Range")
+    private ArrayList<String> llenarContactos() {
+        String proyeccion[] = {
+                ContactsContract.Contacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.Contacts.HAS_PHONE_NUMBER,
+                ContactsContract.Contacts.PHOTO_ID
+        };
+
+        ArrayList<String> lista_contactos_recycle = new ArrayList<>();
+        ContentResolver contentResolver = getActivity().getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                proyeccion, null, null, null);
+        // Con el cursor, recorrer la lista de contactos extraída, agregando los elementos a un ArrayList
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                // Obtener el nombre del contacto
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                // Si tiene teléfono lo agregamos a la lista de contactos
+                if (Integer.parseInt(cursor.getString(
+                        cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    lista_contactos_recycle.add(name);
+                }
+            } // FIN while
+        } // FIN if
+
+        cursor.close();
+        return lista_contactos_recycle;
+    }
+
 }
