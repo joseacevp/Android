acf41c6ea74692ae8843e3ec78f6986bc5169137
Jose Ace <joseacevp@gmail.com>
2024-05-12 20:48:49 +0200

    EXAMEN 2023
    Inicio grabar en base de datos

---
+++ ExamenMayo/ExamenAnterio23/ExamenResolucion23/app/src/main/java/com/example/examenresolucion23/AdaptadorAleatorios.java
@@ -0,0 +1,78 @@
+package com.example.examenresolucion23;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdaptadorAleatorios extends RecyclerView.Adapter<AdaptadorAleatorios.ViewHolderDatos> {
+
+    private ArrayList<Contacto> listaContactos;
+    private Context context;
+
+    public AdaptadorAleatorios(ArrayList<Contacto> listaContactos, Context context) {
+        this.listaContactos = listaContactos;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.linea_contactos_aleatorios, parent, false);
+        return new AdaptadorAleatorios.ViewHolderDatos(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderDatos holder, int position) {
+        holder.setAsignarDatos(listaContactos.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaContactos.size();
+    }
+
+    public class ViewHolderDatos extends RecyclerView.ViewHolder {
+        TextView id, nombre, apellidos, dni;
+        ImageView fotoContacto;
+
+        public ViewHolderDatos(@NonNull View itemView) {
+            super(itemView);
+            id = itemView.findViewById(R.id.id_f);
+            nombre = itemView.findViewById(R.id.texto_nombre_f);
+            apellidos = itemView.findViewById(R.id.texto_apellidos_f);
+            dni = itemView.findViewById(R.id.dni_f);
+            fotoContacto = itemView.findViewById(R.id.foto_f);
+
+        }
+
+        public void setAsignarDatos(Contacto contacto) {
+            id.setText("id: "+contacto.getId());
+            nombre.setText("Nombre: "+contacto.getNombre());
+            apellidos.setText("Apellidos: "+contacto.getApellidos());
+            dni.setText("DNI: "+contacto.getDni());
+            fotoContacto.setImageResource(android.R.drawable.ic_menu_camera);
+
+            fotoContacto.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View v) {
+                                                    Toast.makeText(context, "Guardado: "+contacto.getNombre(), Toast.LENGTH_SHORT).show();
+                                                    
+                                                }
+                                            }
+            );
+        }
+
+
+    }
+}
+
--- ExamenMayo/ExamenAnterio23/ExamenResolucion23/app/src/main/java/com/example/examenresolucion23/ui/crear/CrearFragment.java
+++ ExamenMayo/ExamenAnterio23/ExamenResolucion23/app/src/main/java/com/example/examenresolucion23/ui/crear/CrearFragment.java
@@ -8,13 +8,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.examenresolucion23.AdaptadorAleatorios;
+import com.example.examenresolucion23.AdapterDatos;
+import com.example.examenresolucion23.Contacto;
+import com.example.examenresolucion23.MyDataSet;
 import com.example.examenresolucion23.databinding.FragmentCrearBinding;
+
+import java.util.ArrayList;
 
 
 public class CrearFragment extends Fragment {
 
     private FragmentCrearBinding binding;
+    private ArrayList<Contacto> listaContactoF ;
+    MyDataSet lista = new MyDataSet(25);
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +33,14 @@ public class CrearFragment extends Fragment {
         binding = FragmentCrearBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
+        listaContactoF = lista.ITEMS;
+        System.out.println(listaContactoF.size());
+        binding.recyclerAleatorios.setLayoutManager(new LinearLayoutManager(getContext()
+                , LinearLayoutManager.VERTICAL, false));
+        //15. enviamos la lista con los datos al adaptador
+        AdaptadorAleatorios adapter = new AdaptadorAleatorios(listaContactoF, getContext());
+        //16. indicamos al recycleView que
+        binding.recyclerAleatorios.setAdapter(adapter);
 
 
         return root;
