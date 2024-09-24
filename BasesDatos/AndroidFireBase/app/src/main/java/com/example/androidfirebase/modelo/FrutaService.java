b53adff53673eff98f9192a2cf73a31829a02d5a
Jose Ace <joseacevp@gmail.com>
2024-09-24 17:32:19 +0200

    Inicio JUEGO MULTIPLICACION
    Inicio de el juego realizado para la tarea 4 de PMDM mejorando la funcionalidad.

--- BasesDatos/AndroidFireBase/app/src/main/java/com/example/androidfirebase/modelo/FrutaService.java
+++ BasesDatos/AndroidFireBase/app/src/main/java/com/example/androidfirebase/modelo/FrutaService.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 public class FrutaService {
     public static List<Fruta> frutas = new ArrayList<>();
-    public static void agregarFruta(Fruta fruta){
+
+    public static void agregarFruta(Fruta fruta) {
         frutas.add(fruta);
     }
+    
+
 }
